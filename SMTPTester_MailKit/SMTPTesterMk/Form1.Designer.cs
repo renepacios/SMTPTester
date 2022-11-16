96ba6efde16b85b94576dadef890d84d5e0f5a5d
René Pacios <3912951+renepacios@users.noreply.github.com>
2022-11-16 12:27:46 +0100

    1-avoid-smtpclient-use

---
+++ SMTPTester_MailKit/SMTPTesterMk/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace SMTPTesterMk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.btnProbar = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtForm = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkAnonymous = new System.Windows.Forms.CheckBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbAuto = new System.Windows.Forms.RadioButton();
+            this.rdbUseStartTls = new System.Windows.Forms.RadioButton();
+            this.rdbUseSSL = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServer.Location = new System.Drawing.Point(82, 12);
+            this.txtServer.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(329, 23);
+            this.txtServer.TabIndex = 0;
+            // 
+            // btnProbar
+            // 
+            this.btnProbar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProbar.Location = new System.Drawing.Point(322, 247);
+            this.btnProbar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnProbar.Name = "btnProbar";
+            this.btnProbar.Size = new System.Drawing.Size(88, 27);
+            this.btnProbar.TabIndex = 8;
+            this.btnProbar.Text = "Probar";
+            this.btnProbar.UseVisualStyleBackColor = true;
+            this.btnProbar.Click += new System.EventHandler(this.btnProbar_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(82, 42);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(45, 23);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "587";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // txtForm
+            // 
+            this.txtForm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtForm.Location = new System.Drawing.Point(82, 72);
+            this.txtForm.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtForm.Name = "txtForm";
+            this.txtForm.Size = new System.Drawing.Size(329, 23);
+            this.txtForm.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "From";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTo.Location = new System.Drawing.Point(82, 103);
+            this.txtTo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(329, 23);
+            this.txtTo.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "To";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPass.Enabled = false;
+            this.txtPass.Location = new System.Drawing.Point(82, 249);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(223, 23);
+            this.txtPass.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 253);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Pass";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLogin.Enabled = false;
+            this.txtLogin.Location = new System.Drawing.Point(82, 218);
+            this.txtLogin.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(328, 23);
+            this.txtLogin.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 222);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Login";
+            // 
+            // chkAnonymous
+            // 
+            this.chkAnonymous.AutoSize = true;
+            this.chkAnonymous.Checked = true;
+            this.chkAnonymous.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAnonymous.Location = new System.Drawing.Point(19, 193);
+            this.chkAnonymous.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.chkAnonymous.Name = "chkAnonymous";
+            this.chkAnonymous.Size = new System.Drawing.Size(111, 19);
+            this.chkAnonymous.TabIndex = 5;
+            this.chkAnonymous.Text = "Use anonymous";
+            this.chkAnonymous.UseVisualStyleBackColor = true;
+            this.chkAnonymous.CheckedChanged += new System.EventHandler(this.chkAnonymous_CheckedChanged);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(10, 280);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(400, 213);
+            this.txtLog.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rdbAuto);
+            this.groupBox1.Controls.Add(this.rdbUseStartTls);
+            this.groupBox1.Controls.Add(this.rdbUseSSL);
+            this.groupBox1.Location = new System.Drawing.Point(19, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 54);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Socket Options";
+            // 
+            // rdbAuto
+            // 
+            this.rdbAuto.AutoSize = true;
+            this.rdbAuto.Checked = true;
+            this.rdbAuto.Location = new System.Drawing.Point(13, 22);
+            this.rdbAuto.Name = "rdbAuto";
+            this.rdbAuto.Size = new System.Drawing.Size(51, 19);
+            this.rdbAuto.TabIndex = 2;
+            this.rdbAuto.TabStop = true;
+            this.rdbAuto.Text = "Auto";
+            this.rdbAuto.UseVisualStyleBackColor = true;
+            // 
+            // rdbUseStartTls
+            // 
+            this.rdbUseStartTls.AutoSize = true;
+            this.rdbUseStartTls.Location = new System.Drawing.Point(179, 22);
+            this.rdbUseStartTls.Name = "rdbUseStartTls";
+            this.rdbUseStartTls.Size = new System.Drawing.Size(85, 19);
+            this.rdbUseStartTls.TabIndex = 1;
+            this.rdbUseStartTls.Text = "Use StartTls";
+            this.rdbUseStartTls.UseVisualStyleBackColor = true;
+            // 
+            // rdbUseSSL
+            // 
+            this.rdbUseSSL.AutoSize = true;
+            this.rdbUseSSL.Location = new System.Drawing.Point(89, 22);
+            this.rdbUseSSL.Name = "rdbUseSSL";
+            this.rdbUseSSL.Size = new System.Drawing.Size(65, 19);
+            this.rdbUseSSL.TabIndex = 0;
+            this.rdbUseSSL.Text = "Use SSL";
+            this.rdbUseSSL.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 507);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.chkAnonymous);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtForm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnProbar);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "SMTP Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Button btnProbar;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtForm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkAnonymous;
+        private System.Windows.Forms.TextBox txtLog;
+        private GroupBox groupBox1;
+        private RadioButton rdbUseStartTls;
+        private RadioButton rdbUseSSL;
+        private RadioButton rdbAuto;
+    }
+}
---
+++ SMTPTester_MailKit/SMTPTesterMk/Form1.cs
@@ -0,0 +1,102 @@
+
+
+namespace SMTPTesterMk
+{
+    using MailKit.Net.Smtp;
+    using MailKit.Security;
+    using MimeKit;
+
+    using MimeKit.Text;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnProbar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnviarCorreo();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void chkAnonymous_CheckedChanged(object sender, EventArgs e)
+        {
+            txtLogin.Enabled = !chkAnonymous.Checked;
+            txtPass.Enabled = !chkAnonymous.Checked;
+        }
+
+
+        void log(string msg)
+        {
+            if (string.IsNullOrEmpty(msg)) return;
+            txtLog.Text += Environment.NewLine + msg;
+        }
+
+        private void EnviarCorreo()
+        {
+           
+            try
+            {
+
+
+                //txtForm.Text, txtTo.Text.Trim(), "SMTP Tester", "Mail de prueba"
+                var msg = new MimeMessage();
+                msg.To.Add(MailboxAddress.Parse(txtTo.Text.Trim()));
+                msg.From.Add(MailboxAddress.Parse(txtForm.Text.Trim()));
+                msg.Subject = "SMTP Tester Mk";
+                msg.Body = new TextPart(TextFormat.Plain)
+                {
+                    Text = "Mensaje de prueba"
+                };
+
+                int intPort = 587;
+                int.TryParse(txtPort.Text.Trim(), out intPort);
+
+
+                using var smtp = new SmtpClient();
+
+                SecureSocketOptions socketOptions = SecureSocketOptions.Auto;
+                if (rdbUseSSL.Checked)
+                {
+                    socketOptions = SecureSocketOptions.SslOnConnect;
+                }
+                else if (rdbUseStartTls.Checked)
+                {
+                    socketOptions = SecureSocketOptions.StartTls;
+
+                }
+                
+                smtp.Connect(txtServer.Text.Trim(), intPort, socketOptions);
+                smtp.Authenticate(txtLogin.Text.Trim(), txtPass.Text.Trim());
+                
+                smtp.Send(msg);
+                smtp.Disconnect(true);
+
+                MessageBox.Show(@"Parece que todo guay ;-)");
+            }
+            catch (Exception ex)
+            {
+                log(ex.Message);
+                log(ex.InnerException?.Message);
+                MessageBox.Show("error:" + ex.Message);
+            }
+        }
+    }
+}
